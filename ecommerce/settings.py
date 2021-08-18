6f3854df40ddbaa6f56527f3b9b845bde5036a77
Asmaa Hasnaoui <asmahasnaoui998@gmail.com>
2021-08-18 12:29:51 +0100

    hide secret key

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .secrets import *
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#(*(2d2k4(lh0vxa_0%0cxkt0cdn2r_(gwi9d2@_z)jav3jdg4'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
